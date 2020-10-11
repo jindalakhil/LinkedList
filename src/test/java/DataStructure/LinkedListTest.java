036bcbddd7b8cefc1eda357e1b335330e6033d6d
jindalakhil <akhiljindal222@gmail.com>
2020-10-12 02:15:22 +0530

    [Akhil]Added: LinkedList done in class

---
+++ src/test/java/DataStructure/LinkedListTest.java
@@ -0,0 +1,29 @@
+package DataStructure;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+
+public class LinkedListTest {
+	@Test
+	public void given3NumberWhenAddedToLinkedListShouldBeAddedToTop() {
+		Node<Integer> firstNode = new Node<Integer>(70);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(56);
+		Node<Integer> fourthNode = new Node<Integer>(16);
+		Node<Integer> fifthNode = new Node<Integer>(26);
+		Node<Integer> sixth = new Node<Integer>(20);
+		LinkedList list = new LinkedList();
+		list.add(firstNode);
+		list.add(secondNode);
+		list.add(thirdNode);
+		list.add(fifthNode);
+		list.add(sixth);
+		//list.printNode();
+		list.addInMiddle(fourthNode);
+		list.printNode();
+		list.popLast();
+		list.printNode();
+	}
+}
---
+++ src/test/java/DataStructure/NodeTest.java
@@ -0,0 +1,19 @@
+package DataStructure;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class NodeTest {
+	
+	@Test
+	public void testNode() {
+		Node<Integer> firstNode = new Node<Integer>(56);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(70);
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		boolean check = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
+		Assert.assertTrue(check);
+	}
+}
